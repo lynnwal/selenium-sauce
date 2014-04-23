0e7bb5f142ff3354c2b4d2e3b6101d9918166a94
lynnmedia <lynn@winmedia.com>
2014-04-23 14:31:59 -0700

    added webdriver.js

---
+++ webdriver.js
@@ -0,0 +1,19 @@
+// CISCO CONFIDENTIAL
+// Copyright (c) 2014, Cisco Systems, Inc.
+
+var webdriver = require('/usr/local/lib/node_modules/selenium-webdriver');
+
+var driver = new webdriver.Builder().
+    withCapabilities(webdriver.Capabilities.chrome()).
+    build();
+
+driver.get('http://www.google.com');
+driver.findElement(webdriver.By.name('q')).sendKeys('webdriver');
+driver.findElement(webdriver.By.name('btnG')).click();
+driver.wait(function() {
+    return driver.getTitle().then(function(title) {
+        return title === 'webdriver - Google Search';
+    });
+}, 1000);
+
+driver.quit();
